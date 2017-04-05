1d708892dc9710a147698eb84a6f4bf6487c5a21
Konrad Sikorski <konrad.sikorski@windowslive.com>
2017-04-05 22:32:32 +0200

    update ignore file and assembly info

--- RdpProtocolHandler/Properties/AssemblyInfo.cs
+++ RdpProtocolHandler/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RdpHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Rdp Protocol Handler")]
+[assembly: AssemblyDescription("Handle rdp:// uri in the system and start Remote Desktop Connection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RdpHandler")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Konrad Sikorski")]
+[assembly: AssemblyProduct("Rdp Protocol Handler")]
+[assembly: AssemblyCopyright("Copyright © Konrad Sikorski 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

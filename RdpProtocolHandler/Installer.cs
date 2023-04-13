81e7c28e4375c23d72138a02c764f5f7d81209e6
Konrad Sikorski <konrad.sikorski@windowslive.com>
2023-04-13 22:55:58 +0200

    move code to separate classes

---
+++ RdpProtocolHandler/Installer.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using NLog;
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Security.Principal;
+
+namespace KonradSikorski.Tools.RdpProtocolHandler
+{
+    [SupportedOSPlatform("windows")]
+    internal static class Installer
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private const string REGISTRY_KEY_NAME = "RDP";
+
+        internal static void Uninstall()
+        {
+            ConsoleWrapper.Alloc();
+            if (!RequireAdministratorPrivileges()) return;
+
+            Registry.ClassesRoot.DeleteSubKeyTree(REGISTRY_KEY_NAME, false);
+            ConsoleWrapper.WriteLine("RDP Protocol Handler uninstalled.");
+            Log.Info("RDP Protocol Handler uninstalled.");
+        }
+
+        internal static void Install(bool prompt = true)
+        {
+            ConsoleWrapper.Alloc();
+
+            if (!RequireAdministratorPrivileges()) return;
+
+            //if (prompt)
+            //{
+            //    ConsoleWrapper.Write("Do you want to install RDP Protocol handler? (for details use /?) [Y]es [N]o:");
+            //    var result = ConsoleWrapper.ReadLine();
+            //    if (result?.ToLower() != "y") return;
+            //}
+
+            Uninstall();
+
+            //-- get assembly info
+            var assembly = Assembly.GetExecutingAssembly();
+            var handlerLocation = assembly.Location;
+
+            //-- create registry structure
+            var rootKey = Registry.ClassesRoot.CreateSubKey(REGISTRY_KEY_NAME);
+            var defaultIconKey = rootKey?.CreateSubKey("DefaultIcon");
+            var commandKey = rootKey?.CreateSubKey("shell")?.CreateSubKey("open")?.CreateSubKey("command");
+
+            rootKey?.SetValue("", "rdp:Remote Desktop Protocol");
+            rootKey?.SetValue("URL Protocol", "");
+            defaultIconKey?.SetValue("", @"C:\Windows\System32\mstsc.exe");
+            commandKey?.SetValue("", $@"""{handlerLocation}"" ""%1""");
+
+            //--
+            Log.Info("RDP Protocol Handler installed");
+            ConsoleWrapper.WriteLine("RDP Protocol Handler installed");
+            ConsoleWrapper.WriteLine($"WARNING: Do not move this '{assembly.FullName}' to other location, otherwise handler will not work. If you change the location run installation process again.");
+        }
+
+        private static bool RequireAdministratorPrivileges()
+        {
+            var isAdmin = IsUserAdministrator();
+
+            if (!isAdmin)
+            {
+                var oldColor = ConsoleWrapper.ForegroundColor;
+                ConsoleWrapper.ForegroundColor = ConsoleColor.Red;
+                ConsoleWrapper.WriteLine("You must be system administrator");
+                ConsoleWrapper.ForegroundColor = oldColor;
+                Log.Error("You must be system administrator");
+            }
+
+            return isAdmin;
+        }
+
+        private static bool IsUserAdministrator()
+        {
+            using (var user = WindowsIdentity.GetCurrent())
+            {
+                try
+                {
+                    //get the currently logged in user
+                    var principal = new WindowsPrincipal(user);
+                    return principal.IsInRole(WindowsBuiltInRole.Administrator);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ RdpProtocolHandler/RdpHandler.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+using System.Diagnostics;
+
+namespace KonradSikorski.Tools.RdpProtocolHandler
+{
+    internal class RdpHandler
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        internal static void Run(string parameter)
+        {
+            Log.Debug("Start RDP: " + parameter);
+
+            var rdpAppPath = GetPathToRemoteDesktopApp();
+            var rdpParametersChain = BuildParameters(parameter);
+
+            Process.Start(rdpAppPath, rdpParametersChain);
+            Log.Debug("End RDP");
+        }
+
+        private static string GetPathToRemoteDesktopApp()
+        {
+            return $"{Environment.GetEnvironmentVariable("systemroot")}\\system32\\mstsc.exe";
+        }
+
+        private static string BuildParameters(string parameter)
+        {
+            var uri = parameter.Substring("rdp://".Length).TrimEnd('/');
+            var rdpParameters = uri.Split(',');
+
+            rdpParameters[0] = $"/v:{rdpParameters[0]}";
+            for (int i = 1; i < rdpParameters.Length; i++)
+            {
+                var rdpParam = rdpParameters[i];
+                if (!string.IsNullOrWhiteSpace(rdpParam)) rdpParameters[i] = "/" + rdpParam;
+            }
+
+            var rdpParametersChain = string.Join(" ", rdpParameters);
+            Log.Debug("rdpParametersChain: " + rdpParametersChain);
+            return rdpParametersChain;
+        }
+
+    }
+}
